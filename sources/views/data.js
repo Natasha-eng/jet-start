f3abe7edb08973d88fc4f50d862d0720a2e66b96
Natasha Shilova <natasha.shilova.89@gmail.com>
2024-09-16 13:26:25 +0300

    add items

--- sources/views/data.js
+++ sources/views/data.js
@@ -6,9 +6,29 @@ class CountriesTable extends JetView {
 	config(){
 		return { 
 			view: "datatable",
-			localId: "countriesId",
+			localId:'countriesId',
+			id:"countriesView",
 			select:true,
 			autoConfig: true,
+			editable: true,
+			scrollX: false,
+			columns:[
+				{ id:"Name", header:"Countries",editor: "text",fillspace: true,template: function (obj) {
+					return (
+					  "<div class='space'>" +
+					  obj.Name +
+					  "<span class='removeBtn webix_icon wxi-trash'></span></div>"
+					);
+				  }     },
+				
+			],
+
+			onClick: {
+				removeBtn: function (ev, id) {
+				  this.remove(id);
+				  return false;
+				},
+			  }
 		};
 	}
 	init(){
@@ -21,9 +41,30 @@ class StatusesTable extends JetView {
 	config(){
 		return { 
 			view: "datatable",
-			localId: "statusesId",
+			localId:'statusesId',
+			id:'statusesView',
 			select:true,
-			autoConfig: true,			
+			autoConfig: true,
+			editable: true,
+			fillspace: true,
+			scrollX: false,
+			columns:[
+				{ id:"Name", header:"Statuses",editor: "text",fillspace: true,template: function (obj) {
+					return (
+					  "<div class='space'>" +
+					  obj.Name +
+					  "<span class='removeBtn webix_icon wxi-trash'></span></div>"
+					);
+				  }     },
+				
+			],
+			
+			onClick: {
+				removeBtn: function (ev, id) {
+				  this.remove(id);
+				  return false;
+				},
+			  }		
 		};
 	}
 	init(){
@@ -33,16 +74,53 @@ class StatusesTable extends JetView {
 }
 export default class DataView extends JetView{
 	config(){
+
 		return { 
 			rows: [
-				{view:"tabview", css:"webix_shadow_medium", 
-					cells: [
-						{header:"Countries",  body: CountriesTable },
-						{header:"Statuses",  body: StatusesTable},					
-					] 
-				},
-			
+			{	cols: [
+					{view:"tabbar", id: "tabs", css:"webix_shadow_medium", multiview:true, value:"countriesView",
+						
+							  options: [
+								{ value:'Countries', id:'countriesView'},
+            					{ value:'Statuses', id:'statusesView'},
+							]  								
+					},
+					
+					{view: "form", localId: "add-form", elements: [
+						{
+						  view: "text",
+						  label: "Name",
+						  name: "name",
+						  invalidMessage: "Name must be filled in",
+						},
+						{view: "button",localId: "add-button", value:"Add new", inputWidth:100, click: () => {
+							const form = this.$$("add-form");
+							const isValid = form.validate();
+							if(isValid) {
+								this.addNew()
+								form.clear()
+								form.clearValidation()
+							}							
+						}
+					},
+						
+					  ], 
+					  rules: {
+						name: webix.rules.isNotEmpty,
+					  },}
+				]},
+				{id: "cells",animate: false,cells: [
+					{id:"countriesView",$subview:CountriesTable, name:"countriesView"},
+					{id:"statusesView",$subview:StatusesTable, name: "statusesView"},
+				]},				
 			]
-		};			
+		};	
+			
 	}
+	 addNew (id, ev) {
+		const formValues = this.$$("add-form").getValues();
+		const cellId = this.$$("tabs").getValue()
+
+		const currentTable = this.getRoot().queryView({view: "datatable", id: cellId}).add({"Name":formValues.name})
+	}	
 }
